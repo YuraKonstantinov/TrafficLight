4a64b3129a291789d1968c3984a2ece9011328a2
YuraKonstantinov <georgekonstantinov@yandex.ru>
2021-08-18 22:40:42 +0400

    view controller finish

--- TrafficLight/ViewController.swift
+++ TrafficLight/ViewController.swift
@@ -9,11 +9,35 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    @IBOutlet var colorRedView: UIView!
+    @IBOutlet var colorYellowView: UIView!
+    @IBOutlet var colorGreenView: UIView!
+    
+    @IBOutlet var textButton: UIButton!
+
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        colorRedView.alpha = (0.3)
+        colorYellowView.alpha = (0.3)
+        colorGreenView.alpha = (0.3)
+        colorRedView.layer.cornerRadius = 60
+        colorYellowView.layer.cornerRadius = 60
+        colorGreenView.layer.cornerRadius = 60
+        
+        textButton.layer.cornerRadius = 15
+        
     }
 
+    @IBAction func textButtonPressed() {
+        colorRedView.alpha = 1
+        
+        if colorRedView.alpha == (0.3) {
+            textButton.setTitle("NEXT", for: .normal)
+        }
 
+    }
+    
 }
 
